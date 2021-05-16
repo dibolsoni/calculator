ffe281e13a29d08d6dda92ee4db27036c9184a38
DIEGO CARNAVALLI BOLSONI <dibolsoni@gmail.com>
2021-05-16 20:32:23 -0300

    constants file

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const ERRORS = {
+    auth: {
+        userNotFound: {
+            code: "auth/user-not-found",
+            message: "Email not found. Please, type a valid email."
+        },
+        passwordIncorrect: {
+            code: "auth/wrong-password",
+            message: "Wrong password. Please, type a valid password or reset it."
+        }
+    }
+}
